4cc0343c82168a1cd1ed36f3d7603670be8a1be8
Sergio Blanco <108953777+Serblaor@users.noreply.github.com>
2023-02-16 11:49:03 -0500

    Cambio de CV

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/HV_SB_DE.pdf";
+import pdf from "../../Assets/../Assets/SERGIO_BLANCO_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Serblaor/Portafolio/master/src/Assets/HV_SB_DE.pdf";
+  "https://raw.githubusercontent.com/Serblaor/Portafolio/master/src/Assets/SERGIO_BLANCO_CV.pdf";
 
 
 function ResumeNew() {
